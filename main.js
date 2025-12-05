a249c4f3b64b27697190d500a72885021cfcc237
jtafstn <96604634+jtafstn@users.noreply.github.com>
2025-12-05 09:20:19 +0000

    1205

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
-let gold = 0;
+function loadPage(page) {
+  let content = "";
 
-document.getElementById("btn").onclick = () => {
-  gold++;
-  document.getElementById("gold").innerText = gold;
-};
+  if (page === "page1") {
+    content = `
+      <h2>資源頁面</h2>
+    `;
+  }
+
+  else if (page === "page2") {
+    content = `
+      <h2>建造頁面</h2>
+      <p>可以建造的項目：</p>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    `;
+  }
+
+  else if (page === "page3") {
+    content = `
+      <h2>商店頁面</h2>
+      <p>你可以</p>
+    `;
+  }
+
+  // 將內容塞進右邊容器
+  document.getElementById("contentArea").innerHTML = content;
+}
